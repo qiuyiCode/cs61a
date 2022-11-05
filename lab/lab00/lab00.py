0261252226f71e8781d90b6d63cbed7f44e350f9
tolearn_linux <1030195712@qq.com>
2022-11-05 08:16:08 -0700

    cs61a

---
+++ lab/lab00/lab00.py
@@ -0,0 +1,9 @@
+def twenty_twenty():
+    """Come up with the most creative expression that evaluates to 2020,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty()
+    2018
+    """
+    return 2*10 + 2020 - 2*11
+
